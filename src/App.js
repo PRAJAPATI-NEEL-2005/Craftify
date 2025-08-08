f00c922a183a96cba2d0f37c85d8a695d2fc1ea7
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-08-08 22:04:12 +0530

    main routes and component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Generate from './components/Generate';
+import About from './components/About';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     <p>hello world</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/generate" element={<Generate />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
